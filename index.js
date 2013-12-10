2aecfee56524964705ffa8411c815088ecea4d16
Pedro Morgado <p.morgado107@gmail.com>
2013-12-10 09:20:32 +0100

    Login and register form

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-var express = require('express');
+var express = require('express') , path = require('path');
 var app = express();
  
 app.listen(3000);
+
+app.configure(function(){
+  app.use(express.static(path.join(__dirname, 'public')));
+});
 
 app.get('/', function(request, response) {
    response.sendfile('./views/index.html');
