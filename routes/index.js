cd90021ff50b41574110b9bf6018d8673fa69059
Pedro Morgado <pedro@riderstate.com>
2014-01-15 13:53:44 +0100

    Login and signup errors

--- routes/index.js
+++ routes/index.js
@@ -19,29 +19,37 @@ app.get('/login', function(req, res) {
 });
 
 app.post('/login', function (req, res){
-	console.log('LOGIN');
-	client.post('/auth', {
-		'application_id' : config.application,
-		'client_id' : 'id',
-		'credentials' : {
-			'type' : 'password',
-			'email' : req.body.email,
-			'password' : req.body.password
-		}
-	}, function (err, areq, ares, obj){
-		console.log('ERROR: %s', err);
-		console.dir(obj);
-		if (err) {
-			if (obj.code == "invalid_email_or_password"){
-				req.flash('error', 'Wrong email or password');
-			} else {
-				req.flash('error', 'Unknown login error (' + obj.code + ')')
+	if (!req.body.email){
+		req.flash('error', 'Email can\'t be empty');
+		res.redirect('/login');
+	} else if(!req.body.password){
+		req.flash('error', 'Password can\'t be empty');
+		res.redirect('/login');
+	} else {
+		console.log('LOGIN');
+		client.post('/auth', {
+			'application_id' : config.application,
+			'client_id' : 'id',
+			'credentials' : {
+				'type' : 'password',
+				'email' : req.body.email,
+				'password' : req.body.password
 			}
-			res.redirect('/login');
-		} else {
-			res.redirect('/dashboard');
-		}
-	});
+		}, function (err, areq, ares, obj){
+			console.log('ERROR: %s', err);
+			console.dir(obj);
+			if (err) {
+				if (obj.code == "invalid_email_or_password"){
+					req.flash('error', 'Wrong email or password');
+				} else {
+					req.flash('error', 'Unknown login error (' + obj.code + ')')
+				}
+				res.redirect('/login');
+			} else {
+				res.redirect('/dashboard');
+			}
+		});
+	}
 });
 
 
@@ -53,7 +61,16 @@ app.get('/signup', function(req, res) {
 
 app.post('/signup', function (req, res){
 	console.log('SIGNUP');
-	if(req.body.password == req.body.repeat_password){
+	if (!req.body.email){
+		req.flash('error', 'Email can\'t be empty');
+		res.redirect('/signup');
+	} else if(req.body.password.length < 8){
+		req.flash('error', 'Password needs at least 8 characters');
+		res.redirect('/signup');
+	} else if(req.body.password != req.body.repeat_password){
+		req.flash('error', 'Passwords don\'t match');
+		res.redirect('/signup');
+	} else {
 		client.post('/auth/signup', {
 			'application_id' : config.application,
 			'client_id' : 'id',
@@ -78,9 +95,6 @@ app.post('/signup', function (req, res){
 				res.redirect('/login');
 			}
 		});
-	} else {
-		req.flash('error', 'Passwords don\'t match');
-		res.redirect('/signup');
 	}
 });
 
