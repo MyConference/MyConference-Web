e5080f663c7382a97a0188a85e38eb6b29758857
Daniel Escoz <darkhogg@gmail.com>
2014-03-03 20:58:37 +0100

    Added documents route

---
+++ routes/documents.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var client = require('../client.js');
+
+var config = require('../config.js');
+
+var app = express();
+
+
+// Redirect to login if not logged in
+app.use(function (req, res, next) {
+  if (!req.session.loginData) {
+    return res.redirect('/login');
+  } else {
+    return next();
+  }
+});
