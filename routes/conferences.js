6117276706e808814e893e1a2575c9daab05fb00
Daniel Escoz <darkhogg@gmail.com>
2014-03-03 19:52:24 +0100

    Moved a few templates

--- routes/conferences.js
+++ routes/conferences.js
@@ -28,7 +28,7 @@ app.get('/', function (req, res, next) {
       res.redirect('/'); // FIXME infinite redirect loop
 
     } else {
-      res.render('conferences', {
+      res.render('conferences/index', {
         'conferences': obj
       });
     }
@@ -38,7 +38,7 @@ app.get('/', function (req, res, next) {
 
 app.get('/new', function (req, res, next) {
   // GET - Just render the form
-  res.render('conferences-new', {
+  res.render('conferences/edit', {
     'data': {}
   });
 });
