5628fcee41baf3d0743f3fbd6d6953913e55d83c
Pedro Morgado <p.morgado107@gmail.com>
2014-06-01 11:19:39 +0200

    Map added to venues

---
+++ public/js/googlemaps.js
@@ -0,0 +1,25 @@
+if (google) {
+  google.load('visualization', '1.0', {'packages':['corechart']});
+  
+  google.maps.visualRefresh = true;
+  google.maps.event.addDomListener(window, 'load', function(){
+    var mapOptions = {
+      center: new google.maps.LatLng(40.40, -3.68),
+      zoom: 5,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+
+      panControl: false,
+      zoomControl: true,
+      mapTypeControl: false,
+      scaleControl: false,
+      streetViewControl: false,
+      overviewMapControl: false,
+
+      zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.SMALL
+      }
+    };
+
+    gmap = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  });
+}
